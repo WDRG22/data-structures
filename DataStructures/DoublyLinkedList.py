b41419730b5b6230ed0a97f949d50aa0418e494b
William Gibson <wdr.gibson@gmail.com>
2022-08-02 18:34:15 -0500

    Completed DoublyLinkedList and testing

--- DataStructures/DoublyLinkedList.py
+++ DataStructures/DoublyLinkedList.py
@@ -28,13 +28,26 @@ class DoublyLinkedList:
             current = current.prev
             yield val
             
+    def print_forward(self):
+        for val in self.iter():
+            print(val, end=' ')
+        print()
+            
+    def print_reverse(self):
+        for val in self.reverse_iter():
+            print(val, end=' ')
+        print()
+            
+    def get_count(self):
+        return self.count
+            
     def contains(self, data):
         # Search list for data, return boolean if found
         
         for val in self.iter():
-            if data == val:
+            if val == data:
                 return True
-            return False
+        return False
         
     def append(self, data):
         # Append data to end of list
@@ -61,12 +74,31 @@ class DoublyLinkedList:
         self.count += 1
         
     def delete(self, data):
-        # Delete node containing specified data from list
+        # Delete node containing specified data from list and return its data
+        # Return boolean if node deleted or not
         
-        return
+        if self.head is None:
+            return False
+        elif self.head.data == data:
+            self.delete_head()
+            return True
+        elif self.tail.data == data:
+            self.delete_tail()
+            return True
+        else:
+            current = self.head.next
+            while current:
+                if current.data == data:
+                    current.next.prev = current.prev
+                    current.prev.next = current.next
+                    self.count -= 1
+                    return True
+                current = current.next
+        return False
     
     def delete_head(self):
         # Delete head node and return its data
+        # return None if no Node deleted
         
         if self.head is None:
             return None
@@ -81,7 +113,8 @@ class DoublyLinkedList:
         return val
     
     def delete_tail(self):
-        # Delete tail node and return its data 
+        # Delete tail node and return its data
+        # return None if no Node deleted 
         
         if self.tail is None:
             return None
@@ -94,3 +127,31 @@ class DoublyLinkedList:
             except:
                 pass
         return val
+  
+  
+  
+# TESTING
+  
+greek = [
+        "alpha", "beta", "gamma", "delta", "epsilon",
+         "zeta", "eta", "theta", "iota", "kappa", 
+         "lambda", "mu", "nu", "xi", "omicron", 
+         "pi", "rho", "sigma", "tau", "upsilon",
+         "phi", "chi", "psi", "omega"
+        ]
+
+myList = DoublyLinkedList()
+for letter in greek:
+    myList.append(letter)
+    
+print(myList.get_count()) #24
+print(myList.contains("rho")) #true
+myList.delete("rho")
+print(myList.contains("rho")) #false
+print(myList.delete_head())
+print(myList.delete_tail())
+print(myList.get_count()) #21
+
+myList.insert("NEWHEAD")
+myList.append("NEWTAIL")
+myList.print_forward()
